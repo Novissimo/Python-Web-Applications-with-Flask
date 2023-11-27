12dfcaddcb3d169c569a5f039b976d7fb57a9a48
Novissimo <novobura@gmail.com>
2023-11-28 06:56:16 +0900

    Add files via upload

---
+++ classy.py
@@ -0,0 +1,14 @@
+class Math:
+    notes = "this is a class variable"
+
+    def add(self, num1, num2):
+        print("addition: ", num1 + num2)
+
+    def multiply(self, num1, num2):
+        print("multiplication")
+
+        #create an instance of the class Math
+        #see properties and methods
+        #access class variables
+        #access class methods
+        #check the object type
